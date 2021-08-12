cfc80d1d46a0eeb24c6b08699f2cd37025a3a551
Olatunji <folarin.tunji98@gmail.com>
2021-08-12 04:38:35 +0100

    Teev

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#basic-data-table').DataTable({
+      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+    });
+  });
